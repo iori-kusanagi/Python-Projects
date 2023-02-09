e718268f02556d1752346d300a5aa6a74392ce41
iori-kusanagi <124803628+iori-kusanagi@users.noreply.github.com>
2023-02-09 15:01:33 +0500

    Add files via upload

---
+++ random password generator.py
@@ -0,0 +1,13 @@
+import random
+
+password_characters_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'J', 'K', 'L', 'M',
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', '_', '#']
+
+user = int(input("Enter the number of characters in the password: "))
+
+while random.choice(password_characters_list) != "atifas":
+    for i in range(user):
+        print(random.choice(password_characters_list), end="")
+    print("\n")
+
+print("Finally guessed!")
---
+++ unexisting password generator.py
@@ -0,0 +1,22 @@
+import random
+
+# Have you ever wondered that when we are to make an account on a website, and we choose a password for our account that is already given to another user, the website tells us that we cannot choose that password, and gives suggestions of passwords that are not existing. How does it do that.
+
+# Let's first make a list of passwords that are already existing.
+existing_passwords = ["noser", "bee", "ghost-rider", "killer"]
+
+# Now make a list of characters that a password consists of.
+password_characters_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'J', 'K', 'L', 'M',
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', '_', '#']
+
+user = input("Enter a password: ")
+
+def give_suggestions():
+    for i in range(8):
+        print(random.choice(password_characters_list), end="")
+
+if user in existing_passwords:
+    print("You cannot use that password. You may choose these passwords.")
+    for i in range(8):
+        give_suggestions()
+        print("\n")
